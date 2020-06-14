8fe99134a51025c560b06bc1853f95fe69104172
Mrchipmunk <66779513+Mrchipmunk@users.noreply.github.com>
2020-06-14 08:03:28 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,9 @@ radio.onReceivedNumber(function (receivedNumber) {
     led.toggle(4, 4)
     serial.writeValue("Z", receivedNumber)
 })
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
 basic.showString("Z RECEIVER")
 radio.setGroup(5)
 serial.writeLine("Acceleration")
